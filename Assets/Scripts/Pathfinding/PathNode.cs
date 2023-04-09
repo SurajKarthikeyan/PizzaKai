f661f6b367daba6ad4b567236a949a487a7af985
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2023-04-09 03:36:00 -0400

    Broken pathfinding

---
+++ Assets/Scripts/Pathfinding/PathNode.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode
+{
+    #region VARIABLES
+    private Grid<PathNode> grid;
+    public int x;
+    public int y;
+
+    public int gCost;
+    public int hCost;
+    public int fCost;
+
+    public PathNode cameFromNode;
+
+    #endregion
+    public PathNode(Grid<PathNode> grid, int x, int y) 
+    {
+        this.y = y;
+        this.x = x;
+        this.grid = grid;
+    }
+    public void CalculateFCost() 
+    {
+        fCost = gCost + hCost;
+    }
+    public override string ToString()
+    {
+        return x + "," + y;
+    }
+}
--- Assets/Scripts/Pathfinding/Testing.cs
+++ Assets/Scripts/Pathfinding/Testing.cs
@@ -1,11 +1,34 @@
+using CodeMonkey.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+
+    private Pathfinding pathfinding;
     private void Start()
     {
-        Grid grid = new Grid(4, 2, 10f);
+        pathfinding = new Pathfinding(10,10);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
+            List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
+            if(path != null) 
+            {
+                for(int i = 0; i< path.Count - 1 ;  i++) 
+                { 
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, 
+                        new Vector3(path[i + 1].x, path[i + 1].y) * 10f + Vector3.one * 5f, Color.green, 2, false);
+                }
+            }
+
+
+        }
     }
 }
