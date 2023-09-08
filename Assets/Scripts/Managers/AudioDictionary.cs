687b3f3b33d310cce0d877ef84e356f020b89ecd
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2023-09-07 20:55:47 -0400

    [WIP] Refactored Audio dict system to use new dictionary thats serealizable

--- Assets/Scripts/Managers/AudioDictionary.cs
+++ Assets/Scripts/Managers/AudioDictionary.cs
@@ -16,16 +16,8 @@ public class AudioDictionary : MonoBehaviour
         Molotov,
         Deepdish
     };
+    public UnityDictionary<string, AudioClip> audioDict;
     public Source source;
-    //Please ensure the length of these two lists are identical. It is a must to not have an index out of bounds error
-    [Tooltip("Make the name of the strings here the same as the animation they'd be played in IF there is a relavent animation")]
-    [ReorderableList]
-    [SerializeField] private List<string> aClipKeys;    // keys in dict
-    [ReorderableList]
-    [SerializeField] private List<AudioClip> aClipValues;   // vals in dict
-    [System.NonSerialized] public Dictionary<string, AudioClip> audioDictionary;
-
-
     [Header("Audio Sources")]
     [Tooltip("Audio Source for testing")]
     [SerializeField] public AudioSource testSource;
@@ -39,17 +31,13 @@ public class AudioDictionary : MonoBehaviour
     [SerializeField] public AudioSource molotovSource;
     [Tooltip("Audio Source for the molotov")]
     [SerializeField] public AudioSource deepDishSource;
-
-
     public static AudioDictionary aDict;
-    
     #endregion
 
     #region UnityMethods
     private void Start()
     {
-
-        audioDictionary = new Dictionary<string, AudioClip>();
+        audioDict = new UnityDictionary<string, AudioClip>();
 
         if(aDict == null)
         {
@@ -59,27 +47,13 @@ public class AudioDictionary : MonoBehaviour
         {
             Debug.LogError("You done goofed and made two audio Dictionaries in the scene");  
         }
-
-        MakeDictionary();
     }
     #endregion
-
     #region PrivateMethods
-    public void MakeDictionary()
-    {
-        for (int i = 0; i < aClipKeys.Count; i++)
-        {
-            audioDictionary[aClipKeys[i]] = aClipValues[i]; // assigns the indexed key to the indexed value. Order technically matters 
-            // but thats handled in the inspector anyways so who cares, I just like constant look up time so hash it out with yourself I guess
-            //
-            // get it?
-        }
-    }
-
     // Gets the key along with which source to use and then sets the clip to that source and plays it
     public void PlayAudioClip(string clipKey, Source tempSource)
     {
-        AudioClip tempClip = audioDictionary[clipKey];  // find temporary clip
+        AudioClip tempClip = audioDict[clipKey];  // find temporary clip
         
         switch(tempSource) 
         {
@@ -110,5 +84,16 @@ public class AudioDictionary : MonoBehaviour
         }
     }
     
+    /// <summary>
+    /// Non Local version of Play Audio clip. Instead of using a audio clip attached this this GO, it uses one passed in
+    /// by reference by another GO
+    /// </summary>
+    /// <param name="clipKey"></param>
+    /// <param name="remoteSource"></param>
+    public void PlayAudioClipRemote(string clipKey, AudioSource remoteSource)
+    {
+        remoteSource.clip = audioDict[clipKey];
+        remoteSource.Play();
+    }
     #endregion
 }
