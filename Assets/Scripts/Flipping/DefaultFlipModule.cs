6d6398ae3b3997f63113e4602360dfeeed780846
Zane ODell <odellzan@msu.edu>
2023-09-16 18:03:48 -0400

    Updated DefaultFlipModule
    In an attempt to fix the weapon flipping with previous code to make it easy.

--- Assets/Scripts/Flipping/DefaultFlipModule.cs
+++ Assets/Scripts/Flipping/DefaultFlipModule.cs
@@ -7,7 +7,21 @@ public class DefaultFlipModule : Module
     [ReadOnly]
     [SerializeField]
     protected Vector3 originalScale;
+
+    [Tooltip("Negates the flipping.")]
+    [SerializeField]
+    private bool negate;
+
+    [Tooltip("Whether or not this is flipped.")]
+    [SerializeField]
+    [ReadOnly]
+    private bool flipped;
     #endregion
+
+    /// <summary>
+    /// If <see cref="flipped"/>, then -1. Otherwise, 1.
+    /// </summary>
+    public int FlipMultiplier => flipped ? -1 : 1;
 
     #region Methods
     #region Instantiation
@@ -35,19 +49,23 @@ public class DefaultFlipModule : Module
     /// right.</param>
     public void SetFacingAngle(float theta)
     {
-        Vector3 scale = Master.transform.localScale;
-
         if (theta.AngleIsBetween(-85, 85))
         {
             // Consider the character to be looking to the right.
-            scale.x = originalScale.x;
+            flipped = false;
         }
         else if (theta.AngleIsBetween(95, 265))
         {
             // Consider the character to be looking to the left.
-            scale.x = -originalScale.x;
+            flipped = true;
         }
 
+        // Apply negation.
+        if (negate)
+            flipped = !flipped;
+
+        Vector3 scale = Master.transform.localScale;
+        scale.x = originalScale.x * FlipMultiplier;
         Master.transform.localScale = scale;
     }
     #endregion
