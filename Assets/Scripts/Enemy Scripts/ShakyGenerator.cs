b0fd6b22113c4b4d16e05580d2686b5eebf2d4d0
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2023-09-18 20:00:30 -0400

    shaking generator script

---
+++ Assets/Scripts/Enemy Scripts/ShakyGenerator.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakyGenerator : MonoBehaviour
+{
+    [Tooltip("The amount of distance from the normal location that the shaking can occur")]
+    public float shakeAmount = 0.05f;
+
+    Vector2 normalPos;
+    Vector2 shakyPos;
+
+    bool doShake;
+    float shakeDuration;
+
+    private void Start()
+    {
+        normalPos = gameObject.transform.position;
+        //startShaking(5f);
+    }
+    void FixedUpdate()
+    {
+        if (doShake)
+        {//randomize x and y values 
+            shakyPos = new Vector2(Random.Range(normalPos.x -shakeAmount,normalPos.x + shakeAmount), Random.Range(normalPos.y -shakeAmount,normalPos.y + shakeAmount));
+
+            gameObject.transform.position = shakyPos;
+        }
+    }
+
+    //this is what would be called from other scripts
+    public void startShaking(float duration)
+    {
+        shakeDuration = duration;
+        StartCoroutine("Shaking");
+       
+    }
+
+    private IEnumerator Shaking()
+    {
+        doShake = true;
+
+        yield return new WaitForSeconds(shakeDuration);
+
+        doShake = false;
+        gameObject.transform.position = normalPos;
+    }
+
+}
