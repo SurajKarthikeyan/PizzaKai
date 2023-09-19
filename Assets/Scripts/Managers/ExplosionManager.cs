8dec91a4ca1b132dc97ca33ae6381eb0d13eedb1
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2023-09-19 19:44:18 -0400

    added rotation to explosions

--- Assets/Scripts/Managers/ExplosionManager.cs
+++ Assets/Scripts/Managers/ExplosionManager.cs
@@ -16,10 +16,11 @@ public class ExplosionManager : MonoBehaviour
         instance = this;
     }
 
-    public static GameObject SelectExplosion(Vector2 spawnPoint)
+    public static GameObject SelectExplosion(Vector2 spawnPoint, float angleRotation)
     {
         GameObject explosionChoice = instance.explosionsList[Random.Range(0, instance.explosionsList.Count)];
         explosionChoice.transform.position = spawnPoint;
+        explosionChoice.transform.rotation = Quaternion.Euler(0, 0, angleRotation);
         return explosionChoice;
     }
 }
