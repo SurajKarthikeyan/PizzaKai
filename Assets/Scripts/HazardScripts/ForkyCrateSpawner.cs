8767d31181e01a396551216fd9764d4647990723
Eli Mcardle <elimcardle2001@gmail.com>
2023-09-19 00:35:10 -0400

    crate stack prefabs

--- Assets/Scripts/HazardScripts/ForkyCrateSpawner.cs
+++ Assets/Scripts/HazardScripts/ForkyCrateSpawner.cs
@@ -6,10 +6,19 @@ public class ForkyCrateSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] cratePrefabs;
     [SerializeField] private Vector3 spawnPos;
+    [SerializeField] private GameObject oilCrate;
 
     public void SpawnCrate(int numCrates = 1, bool hasOil = false)
     {
         GameObject crateGO = Instantiate<GameObject>(cratePrefabs[numCrates]);
         crateGO.transform.position = spawnPos;
+
+        if(hasOil)
+        {
+            GameObject randCrate = crateGO.transform.GetChild(Random.Range(0, numCrates + 1)).gameObject;
+            GameObject oilCrateGO = Instantiate<GameObject>(oilCrate);
+            oilCrateGO.transform.position = randCrate.transform.position;
+            Destroy(randCrate);
+        }
     }
 }
