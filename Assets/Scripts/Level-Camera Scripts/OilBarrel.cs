d6a477c75fa87795c97f716f230ecbb4a96a63ee
Eli Mcardle <elimcardle2001@gmail.com>
2023-09-23 17:33:20 -0400

    Fixed crates and oil barrels

--- Assets/Scripts/Level-Camera Scripts/OilBarrel.cs
+++ Assets/Scripts/Level-Camera Scripts/OilBarrel.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class OilBarrel : MonoBehaviour
 {
+    public float explosionRadius = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
         //Destroy(gameObject);
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "PlayerFireBullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     private void OnDestroy()
     {
         Vector2 explosionPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        Physics2D.OverlapCircle(gameObject.transform.position, explosionRadius);
         Instantiate(ExplosionManager.explosionManager.SelectExplosionRandom(explosionPos, -90));
     }
 }
