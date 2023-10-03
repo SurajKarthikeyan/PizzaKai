77f8f6ef08b9a6d8489e7f3680ef5f071a9752ee
Ryan Chang <changry3@msu.edu>
2023-10-03 13:41:48 -0400

    CM_ConnectToPlayer

---
+++ Assets/Scripts/UI-Game Manager Scripts/CM_ConnectToPlayer.cs
@@ -0,0 +1,29 @@
+﻿using Cinemachine;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CM_ConnectToPlayer : MonoBehaviour
+{
+    public CinemachineVirtualCamera vCam;
+
+    public PlayerControlModule pcm;
+
+    private void Reset()
+    {
+        SetVars();
+    }
+
+    private void Awake()
+    {
+        SetVars();
+    }
+
+    private void SetVars()
+    {
+        this.RequireComponent(out vCam);
+
+        GameObject.FindGameObjectWithTag("Player")
+            .RequireComponent(out pcm);
+    }
+}
