d774cb843235781750ee7b4968c24b2234e8ad1b
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2023-10-07 22:24:54 -0400

    Ravioli now screams when he dies

--- Assets/Scripts/Enemy Scripts/RavioliPathing.cs
+++ Assets/Scripts/Enemy Scripts/RavioliPathing.cs
@@ -10,8 +10,7 @@ public class RavioliPathing : Character
 
     [SerializeField] private AudioSource ravioliScream;
     [SerializeField] private AudioSource ravioliExplode;
-    [FormerlySerializedAs("destoryDelay")]
-    [Tooltip("Set to 0.4 seconds because that was enough delay to play audio source and destroy GO without looking bad. Do not change unless smart")]
+    [Tooltip("Set to 0.65 seconds because that was enough delay to play audio source and destroy GO without looking bad. Do not change unless smart")]
     [SerializeField] private float destroyDelay;
     #endregion
 
@@ -19,16 +18,18 @@ public class RavioliPathing : Character
 
     public override void Die()
     {
+        //visual syncing - just disabled the SR so it looks like the sprite is gone to make it easier
+        // This all happens in a span of under a second anyways so I'm not really worried
+        this.GetComponent<SpriteRenderer>().enabled = false;
         StartCoroutine(WaitForSound(destroyDelay));
     }
 
     IEnumerator WaitForSound(float delay)
     {
+        // Play audio, wait then destory
         AudioDictionary.aDict.PlayAudioClipRemote("ravioliScream", ravioliScream);
-        //yield return new WaitWhile(() => ravioliScream.isPlaying);
         AudioDictionary.aDict.PlayAudioClipRemote("ravioliExplode", ravioliExplode);
         Instantiate(ExplosionManager.explosionManager.SelectExplosionRandom(this.gameObject.transform.position, 0f));
-        //yield return new WaitWhile(() => ravioliExplode.isPlaying);
         yield return new WaitForSeconds(delay);
         Destroy(this.gameObject);
     }
