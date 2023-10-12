dc6d87d362c00cbbf4b9ac0c1373faff599566d8
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2023-10-12 01:40:43 -0400

    Lever can be shot has cool down to show it is "actiave" and when it "unable to be shot". These can be changed but it does its  job

--- Assets/Scripts/InteractableEnviorment/Lever.cs
+++ Assets/Scripts/InteractableEnviorment/Lever.cs
@@ -7,7 +7,12 @@ public class Lever : MonoBehaviour
 {
     #region Variables
 
-    public GameObject leverArm; // This is simply a test variable. It might be the case wherein we have a different way to showcase that the lever was activated
+    [SerializeField] private GameObject leverArm; // This is simply a test variable. It might be the case wherein we have a different way to showcase that the lever was activated
+    [Tooltip("Time wherein the lever is unable to be shot again and is deactivated")]   // goes magenta 
+    [SerializeField] private float disabledTime;
+    [Tooltip("Time wherein the lever is unable to be shot again but is activated")] // stays red
+    [SerializeField] private float enabledTime;
+    private bool canBeShot = true;
     
 
     #endregion
@@ -16,8 +21,31 @@ public class Lever : MonoBehaviour
 
     public void LeverArmActivate()
     {
-        AudioDictionary.aDict.PlayAudioClip("leverSwitch", AudioDictionary.Source.Lever);
+        if (canBeShot)
+        {
+            AudioDictionary.aDict.PlayAudioClip("leverSwitch", AudioDictionary.Source.Lever);
+            StartCoroutine(EnabledTime());
+        }
+    }
+
+    IEnumerator EnabledTime()
+    {
+        //Active, unable to be shot again
+        canBeShot = false;
         leverArm.GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(disabledTime);
+        StartCoroutine(DisabledTime());
+    }
+
+    IEnumerator DisabledTime()
+    {
+        //Not active, unable to be shot again
+        leverArm.GetComponent<SpriteRenderer>().color = Color.magenta;
+        //------------------Play different SFX here???????--------------------//
+        yield return new WaitForSeconds(enabledTime);
+        leverArm.GetComponent<SpriteRenderer>().color = Color.white;
+        canBeShot = true;
+        //------------------Play Enable SFX here???????--------------------//
     }
 
     #endregion
