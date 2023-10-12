54c7bc8424f9967349ed034ac1c779cdfaeaae62
Ryan Chang <changry3@msu.edu>
2023-10-12 12:41:01 -0400

    Moved some files around

---
+++ Assets/Scripts/Character/Module/Enemy/Editor/EnemyAITreeDrawerUIE.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(EnemyAITree))]
+public class EnemyAITreeDrawerUIE : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Draw the root.
+        property.Next(true);
+        EditorGUI.PropertyField(position, property, label, true);
+
+        EditorGUI.EndProperty();
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        property.Next(true);
+        return EditorGUI.GetPropertyHeight(property);
+    }
+}
