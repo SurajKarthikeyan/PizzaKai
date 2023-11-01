0da8c7b5d5394fd11e5fd4154a15adf2dd1325d0
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2023-10-31 20:07:55 -0400

    got rid of bad ai

--- Assets/Scripts/Pathfinding/PathfindingData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Custom/Scriptable Objects/PathfindingData")]
-public class PathfindingData : ScriptableObject
-{
-    public Graph<Vector3Int> pathData;
-}
