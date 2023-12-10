47440d2e61af5a0c0e220c9ffcd01f1c71453d83
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2023-12-10 14:15:19 -0500

    beginnings of slam debris

--- Assets/Scripts/Full Weapons/PlayerAnimationEvents.cs
+++ Assets/Scripts/Full Weapons/PlayerAnimationEvents.cs
@@ -9,6 +9,8 @@ public class PlayerAnimationEvents : MonoBehaviour
     public CharacterMovementModule playerCharacterMovementModule;
     public ShotGunWeapon playerShotGunWeapon;
     public SpriteRenderer shotgunSpriteRenderer;
+    public GameObject debris;
+    public Transform debrisSpawnpoint;
     public void DisableInputs()
     {
         playerCharacterMovementModule.canInput = false;
@@ -21,5 +23,10 @@ public class PlayerAnimationEvents : MonoBehaviour
         playerCharacterMovementModule.canInput = true;
         playerShotGunWeapon.shotgunDashing = false;
         shotgunSpriteRenderer.enabled = true;
+    }
+    public void SpawnDebris()
+    {
+        GameObject _debris = Instantiate(debris);
+        _debris.transform.position = debrisSpawnpoint.position;
     }
 }
