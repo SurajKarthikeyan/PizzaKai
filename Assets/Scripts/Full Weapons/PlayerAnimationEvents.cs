9655f41c9b3da6b5e112825a0853fbcbed3498f4
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2023-12-10 15:31:20 -0500

    First half and idle aspect of emission work for shotgun dash

--- Assets/Scripts/Full Weapons/PlayerAnimationEvents.cs
+++ Assets/Scripts/Full Weapons/PlayerAnimationEvents.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -11,11 +12,28 @@ public class PlayerAnimationEvents : MonoBehaviour
     public SpriteRenderer shotgunSpriteRenderer;
     public GameObject debris;
     public Transform debrisSpawnpoint;
+
+    public Material playerMat;
+    public Texture2D glowTexDashStart;
+    public Texture2D glowTexDashSlam;
+    public Texture2D playerDefault;
+    private static readonly int GlowTex = Shader.PropertyToID("_GlowTex");
     public void DisableInputs()
     {
+        playerMat.SetTexture(GlowTex, glowTexDashStart);
         playerCharacterMovementModule.canInput = false;
         playerShotGunWeapon.shotgunDashing = true;
         shotgunSpriteRenderer.enabled = false;
+    }
+
+    public void SetSlamEmission()
+    {
+        playerMat.SetTexture(GlowTex, playerDefault);
+    }
+
+    public void RemoveGlowTex()
+    {
+        playerMat.SetTexture(GlowTex, playerDefault);
     }
 
     public void EnableInput()
@@ -23,6 +41,7 @@ public class PlayerAnimationEvents : MonoBehaviour
         playerCharacterMovementModule.canInput = true;
         playerShotGunWeapon.shotgunDashing = false;
         shotgunSpriteRenderer.enabled = true;
+        playerMat.SetTexture(GlowTex, playerDefault);
     }
     public void SpawnDebris()
     {
